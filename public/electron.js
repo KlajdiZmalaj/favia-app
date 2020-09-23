9efc5cb1c7d2827a8c776e0717e5854b378dbdeb
Klajdi <klajdizmalaj@icloud.com>
2020-09-23 18:48:42 +0200

    m

---
+++ public/electron.js
@@ -0,0 +1,36 @@
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require("path");
+const isDev = require("electron-is-dev");
+let mainWindow;
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 680,
+    minHeight: 680,
+    minWidth: 1024,
+  });
+
+  mainWindow.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../build/index.html")}`
+  );
+  if (!isDev) {
+    mainWindow.removeMenu();
+  }
+
+  mainWindow.on("closed", () => (mainWindow = null));
+}
+app.on("ready", createWindow);
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+app.on("activate", () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
